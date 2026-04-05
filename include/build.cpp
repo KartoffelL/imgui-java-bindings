49430aa58d1c2cdf9d937c4223095079f85dee0e
Kartoffel Licht <84570564+KartoffelL@users.noreply.github.com>
2026-04-05 11:04:46 +0200

    Update build.cpp

--- include/build.cpp
+++ include/build.cpp
@@ -1,7 +1,7 @@
 
+// Dear ImGui files
 #include "imgui/imgui.cpp"
 #include "imgui/imgui_demo.cpp"
-#include "imgui/imgui_internal.cpp"
 #include "imgui/imgui_draw.cpp"
 #include "imgui/imgui_tables.cpp"
 #include "imgui/imgui_widgets.cpp"
