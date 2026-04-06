2bf58773c0658ed84416de310edc21ac84fb62c4
Kartoffel Licht <84570564+KartoffelL@users.noreply.github.com>
2026-04-06 17:48:39 +0200

    Update build.cpp

--- include/build.cpp
+++ include/build.cpp
@@ -6,5 +6,13 @@
 #include "imgui/imgui_tables.cpp"
 #include "imgui/imgui_widgets.cpp"
 
+// Dear ImGui backend
+
+#include "glfw/include/GLFW/glfw3.h"
+#include "glfw/include/GLFW/glfw3native.h"
+
+#include "imgui/backends/imgui_impl_opengl3.cpp"
+#include "imgui/backends/imgui_impl_glfw.cpp"
+
 //ImGuiColorTextEdit files
 #include "ImGuiColorTextEdit/TextEditor.cpp"
