f3b60b3d3ed6908f3114c8f9b290e7e84c5c7f02
Kartoffel Licht <84570564+KartoffelL@users.noreply.github.com>
2026-04-08 15:24:11 +0200

    Update build.h

--- include/build.h
+++ include/build.h
@@ -1,1 +1,8 @@
+#include "imconfig.h" //Custom configuration for builds
 
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+
+#include "ImGuiColorTextEdit/TextEditor.h"
+
+#include "imgui/misc/freetype/imgui_freetype.h"
